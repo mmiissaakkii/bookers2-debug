16681555c4186dff2f947e9442e1ec748975aba3
mmiissaakkii <mmmnn1228@gmail.com>
2022-12-03 09:13:13 +0000

    1303

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'relationships/followings'
   get 'relationships/followers'
+  get 'search' => 'searches#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   get "home/about"=>"homes#about"
@@ -10,13 +12,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create,:destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
